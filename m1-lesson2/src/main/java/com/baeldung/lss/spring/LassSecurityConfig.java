9b94d1295927bb7514b676427352a54c195506ba
omacarena <omacarena@gmail.com>
2017-08-18 16:42:48 +0300

    m1-lesson2 :: add in-memory authentication

---
+++ m1-lesson2/src/main/java/com/baeldung/lss/spring/LassSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.baeldung.lss.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Extends a configuration class which provides some defaults
+ */
+@EnableWebSecurity
+public class LassSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("user")
+			.password("password")
+			.roles("USER");
+	}
+}
